353f6891fe38da0eefa8419386764ec12afc1541
Ben Kiggen and Luke Vandekieft <ben@>
2018-08-09 13:18:56 -0700

    add h6 to image, add gender

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,25 @@ $(document).ready(function() {
     var q2 = $('input[name=quest2]:checked').val();
     var q3 = $('input[name=quest3]:checked').val();
     var q4 = $('input[name=quest4]:checked').val();
+    var gen = $('input[name=gen1]:checked').val();
     var score = parseInt(q1) + parseInt(q2) + parseInt(q3) + parseInt(q4);
     console.log(score);
 
     if(score <= 20){
-      $("img").hide();
-      $("img.low").show();
+      $("img, h6").hide();
+      $(".low").show();
     } else if(score > 20 && score <= 50) {
-      $("img").hide();
-      $("img.medium").show();
+      $("img, h6").hide();
+      $(".medium").show();
     } else {
-      $("img").hide();
-      $("img.high").show();
+      $("img, h6").hide();
+      $(".high").show();
+    }
+
+    if(gen === "male"){
+      $(".women").hide();
+    } else {
+      $(".men").hide();
     }
   });
 });
