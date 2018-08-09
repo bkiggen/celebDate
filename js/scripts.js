1850cb3722d812d068f675541686d36506bdda4e
Ben Kiggen and Luke Vandekieft <luke@>
2018-08-09 12:03:44 -0700

    build html, js to handle form functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    event.preventDefault();
+    var q1 = $('input[name=quest1]:checked').val();
+    var q2 = $('input[name=quest2]:checked').val();
+    var q3 = $('input[name=quest3]:checked').val();
+    var q4 = $('input[name=quest4]:checked').val();
+    var score = parseInt(q1) + parseInt(q2) + parseInt(q3) + parseInt(q4);
+    console.log(score);
 
+    if(score <= 20){
+      $("img").hide();
+      $("img.low").show();
+    } else if(score > 20 && score <= 50) {
+      $("img").hide();
+      $("img.medium").show();
+    } else {
+      $("img").hide();
+      $("img.high").show();
+    }
+  });
+});
